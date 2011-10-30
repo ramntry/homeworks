dddc760473b3ef8d9b81389ecdb13711dc31d57a
Roman Tereshin <ramntry@gmail.com>
2011-10-30 19:05:55 +0400

    Init

---
+++ semester1/hw06/task01/hw.06_task.01.cpp
@@ -0,0 +1,16 @@
+// author: Roman Tereshin
+// email:  roman.tereshin@student.spbu.ru
+
+// hw 06, task 01
+
+// full condition of the problem...
+
+// [time start] 18:27 30.10.11
+// [time estimate] hh:mm 00
+
+
+// SOLUTION CODE 
+
+
+// [time done] hh:mm 30.10.11
+// [time real] hh:mm 00
---
+++ semester1/hw06/task01/task01.pro
@@ -0,0 +1,3 @@
+SOURCES += \
+    hw.06_task.01.cpp
+
