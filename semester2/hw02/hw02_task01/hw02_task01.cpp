7a96f519df23056000e9382af057145d1177744d
Roman Tereshin <ramntry@gmail.com>
2012-02-26 16:46:31 +0400

    main is modified

--- semester2/hw02/hw02_task01/hw02_task01.cpp
+++ semester2/hw02/hw02_task01/hw02_task01.cpp
@@ -10,6 +10,7 @@ int main()
     List *list2 = new ArrayList();
     list->insert(0, 10);
     list->remove(0);
+    list2->insert(0, 20);
     cout << "length: " << list->length() << " - " << list2->length() << endl;
 
     delete list;
