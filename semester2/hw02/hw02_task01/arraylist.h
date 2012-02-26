8b7474d8f13bb1bbef7524ad64c79922faf76f8a
Roman Tereshin <ramntry@gmail.com>
2012-02-26 20:59:08 +0400

    The order of the class members is corrected in accordance with the styleguide

--- semester2/hw02/hw02_task01/arraylist.h
+++ semester2/hw02/hw02_task01/arraylist.h
@@ -5,12 +5,12 @@ class ArrayList : public List
 {
 public:
     ArrayList();
+    ~ArrayList();
     void insert(int position, ListItem item);
     void remove(int position);
     int find(ListItem item);
     int length();
     ListItem & operator [](int position);
-    ~ArrayList();
 
 private:
     int mLength;
--- semester2/hw02/hw02_task01/linkedlist.h
+++ semester2/hw02/hw02_task01/linkedlist.h
@@ -5,12 +5,12 @@ class LinkedList : public List
 {
 public:
     LinkedList();
+    ~LinkedList();
     void insert(int position, ListItem item);
     void remove(int position);
     int find(ListItem item);
     int length();
     ListItem & operator [](int position);
-    ~LinkedList();
 
 private:
     int mLength;
