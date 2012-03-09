8ce3002a1c7f401699fcb11f559ac6703f79ac4b
Roman Tereshin <ramntry@gmail.com>
2012-03-10 00:03:08 +0400

    Comparator's test was been upgraded

--- semester2/hw03/hw03_task01/hw03_task01.cpp
+++ semester2/hw03/hw03_task01/hw03_task01.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
+#include <string>
 #include "comparator.h"
 
 using namespace std;
 
 int main()
 {
-    cout << StandartComparator<double>(0.001)(2.0/3.0, 2.01/3.0) << endl;
+    cout << StandartComparator<int>()(5, 2) << endl;
+    cout << StandartComparator<string>()("hi", "hi") << endl;
+    cout << StandartComparator<const char*>()("hello", "world") << endl;
+
+    Comparator<double> *comparators[3];
+    comparators[0] = new StandartComparator<double>;
+    comparators[1] = new StandartComparator<double>(0.01);
+    comparators[2] = new StandartComparator<double>(0.001);
+
+    for (int i = 0; i < 3; i++)
+        cout << (*comparators[i])(2.0/3.0, 2.01/3.01) << endl;
 
     return 0;
 }
