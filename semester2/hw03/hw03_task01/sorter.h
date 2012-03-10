470551bdc481ef443a7964c028a2b375d2ccdc2d
Roman Tereshin <ramntry@gmail.com>
2012-03-10 13:50:26 +0400

    Problems with inheritance templates in hw03_task01 managed to avoid (not solve)

--- semester2/hw03/hw03_task01/sorter.h
+++ semester2/hw03/hw03_task01/sorter.h
@@ -17,10 +17,13 @@ public:
         : mComparator(new C())
     {}
     ~Sorter() { delete mComparator; }
-    virtual void sort(T* data, size_t size) = 0;
     C &comparator() { return *mComparator; }
 
+    void sort(T* data, size_t size, Comparator<T> *comp = 0)
+    { _sort(data, size, comp ? *comp : *mComparator); }
+
 protected:
+    virtual void _sort(T* data, size_t size, Comparator<T> &comp) = 0;
     C *mComparator;
 };
 
@@ -29,23 +32,11 @@ template <typename T, typename C = StandartComparator<T> >
 class BubbleSorter : public Sorter<T, C>
 {
 public:
-    BubbleSorter()
-        : Sorter<T, C>()
-    {}
-
-    void sort(T* data, size_t size)
+    void _sort(T* data, size_t size, Comparator<T> &comp)
     {
         for (int i = size - 1; i > 0; --i)
             for (int j = 0; j < i; j++)
-        // ERROR:     В строке [1] я не могу обратиться к защищенному полю родительского класса - его, несмотря на
-        //        наследование, просто нет в текущей области видимости.
-        //            В строке [2], очевидно, сохраняется та же проблема, но сообщение о ней перекрывается другой
-        //        ошибкой - не удается вывод шаблона. Совершенно непонятно, почему.
-        //            В строках [3] и [4] - рабочие и равнозначные варианты
-        //        if ((*mComparator)(data[j], data[j + 1]) > 0)               // [1]
-        //        if (comparator()(data[j], data[j + 1]) > 0)                 // [2]
-                if (Sorter<T, C>::comparator()(data[j], data[j + 1]) > 0)   // [3]
-        //        if ((*Sorter<T, C>::mComparator)(data[j], data[j + 1]) > 0) // [4]
+                if (comp(data[j], data[j + 1]) > 0) // [4]
                     swap(data[j], data[j + 1]);
     }
 };
