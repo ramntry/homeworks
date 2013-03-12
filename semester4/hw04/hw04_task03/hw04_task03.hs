1ccf5dfbc795a83b3054a09cf57a6eaecd7fa12f
ramntry <ramntry@gmail.com>
2013-03-12 21:17:25 +0400

    task 03 completed

---
+++ semester4/hw04/hw04_task03/hw04_task03.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import Test.QuickCheck
+
+countEvens :: (Integral a) => [a] -> Int
+countEvens = foldr step 0
+	where step x | even x = (+ 1)
+	             | otherwise = id
+
+countEvens2 :: (Integral a) => [a] -> Int
+countEvens2 = length . filter even
+
+countEvens3 :: (Integral a) => [a] -> Int
+countEvens3 = sum . map count
+	where count x | even x = 1
+	              | otherwise = 0
+
+main = quickCheck ((\xs ->
+		let count = countEvens xs
+		in countEvens2 xs == count && count == countEvens3 xs) :: [Int] -> Bool)
