a202de2c7ca86f32be19c37870a8408d72255746
ramntry <ramntry@gmail.com>
2013-03-12 21:46:27 +0400

    fix whitespaces

--- semester4/hw04/hw04_task03/hw04_task03.hs
+++ semester4/hw04/hw04_task03/hw04_task03.hs
@@ -4,17 +4,17 @@ import Test.QuickCheck
 
 countEvens :: (Integral a) => [a] -> Int
 countEvens = foldr step 0
-	where step x | even x = (+ 1)
-	             | otherwise = id
+    where step x | even x = (+ 1)
+                 | otherwise = id
 
 countEvens2 :: (Integral a) => [a] -> Int
 countEvens2 = length . filter even
 
 countEvens3 :: (Integral a) => [a] -> Int
 countEvens3 = sum . map count
-	where count x | even x = 1
-	              | otherwise = 0
+    where count x | even x = 1
+                  | otherwise = 0
 
 main = quickCheck ((\xs ->
-		let count = countEvens xs
-		in countEvens2 xs == count && count == countEvens3 xs) :: [Int] -> Bool)
+        let count = countEvens xs
+        in countEvens2 xs == count && count == countEvens3 xs) :: [Int] -> Bool)
