bea7a9ebb00d6e60ff262f406f4ee200a01fab58
ramntry <ramntry@gmail.com>
2013-05-16 19:45:06 +0400

    task 04, hw05 completed

--- semester4/hw05/hw05_task04/hw05_task04.hs
+++ semester4/hw05/hw05_task04/hw05_task04.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import Test.QuickCheck
+
 pairProduct :: Int -> [Int]
-pairProduct n = let nums = [1..n] in nums >>= \x -> nums >>= return . (* x)
+pairProduct n = l >>= \x -> l >>= \y -> return (x * y)
+    where l = [1..n]
+
+prop_size n = n >= 0 ==> length (pairProduct n) == n * n
+prop_sum n = n >= 0 ==> sum (pairProduct n) == n^2 * (n + 1)^2 `div` 4
+
+main = do
+    quickCheckWith stdArgs { maxSize = 15 } prop_size
+    quickCheckWith stdArgs { maxSize = 15 } prop_sum
